822516186846c5897774f255ed495bade7688b2c
htaulien <hardy.taulien@accenture.com>
2021-10-18 13:43:52 +0200

    added main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+resource "google_project_service" "this" {
+    for_each           = toset(var.services-list)
+    
+    disable_on_destroy = false
+    project            = var.project-id
+    
+    service            = each.value
+}
+
+resource "google_cloudbuild_trigger" "build-trigger" {
+  trigger_template {
+    branch_name = "master"
+    repo_name   = "https://github.com/ht-accenture/gcpbuildtriggertest"
+  }
+
+  build {
+    step {
+      name = "gcr.io/cloud-builders/gsutil"
+      args = ["mb", "gs://test-bucket-hopefully-created-with-tf"]
+      timeout = "120s"
+    }
+  }  
+}
